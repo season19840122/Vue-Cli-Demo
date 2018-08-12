f6781883007836de30d1d639f5ec665315d9b65a
season19840122 <season19840122@163.com>
2018-08-12 23:31:34 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
       // 模板来源
       template: 'public/index.html',
       // 在 dist/index.html 的输出
-      filename: '金币回收填单页.html',
+      filename: 'index.html',
       // 当使用 title 选项时，
       // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
       title: '金币回收填单页',
